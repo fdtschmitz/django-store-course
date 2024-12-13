c65589ef3aa7974471df651114a8a917e3e0a15d
Fernando Schmitz <fdtschmitz.dev@gmail.com>
2024-12-12 21:25:29 -0300

    Aula 37

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store.views.categories',
+                'store.views.categories', #
+                'cart.context_processors.cart', #
 
             ],
         },
