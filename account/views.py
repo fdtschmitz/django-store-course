134344bad148c9d3a0bd610360f044ca68fdb176
Fernando DT Schmitz <fdtschmitz.dev@gmail.com>
2024-12-27 07:39:35 -0300

    Aula 66

--- account/views.py
+++ account/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.models import User
+from . token import user_tokenizer_generate
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
 
 
 
@@ -13,12 +20,79 @@ def register(request):
 
         if form.is_valid():
 
-            form.save()
+            user = form.save()
 
-            return redirect('store')
+            user.is_active = False
+
+            user.save()
+
+            # Email verification setup
+
+            current_site = get_current_site(request)
+
+            subject = 'Account verification email'
+
+            message = render_to_string('account/registration/email-verification.html', {
+
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+
+            })
+
+            user.email_user(subject=subject, message=message)
+
+            
+
+            return redirect('email-verification-sent')
+
+
         
     context = {'form':form}
 
 
 
     return render(request, 'account/registration/register.html', context)
+
+def email_verification(request, uidb64, token):
+
+    try:
+
+        unique_id = force_str(urlsafe_base64_decode(uidb64))
+
+        user = User.objects.get(pk=unique_id)
+
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+
+        user = None
+
+    if user is not None and user_tokenizer_generate.check_token(user, token):
+
+        user.is_active = True
+
+        user.save()
+
+        return redirect('email-verification-success')
+
+    else:
+
+        return redirect('email-verification-failed')
+
+    
+
+
+def email_verification_sent(request):
+
+    return render(request, 'account/registration/email-verification-sent.html')
+
+
+def email_verification_success(request):
+
+    return render(request, 'account/registration/email-verification-success.html')
+
+
+def email_verification_failed(request):
+
+    return render(request, 'account/registration/email-verification-failed.html')
+
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,24 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+
+# Email Host User and Password
+# All information are stored in .env file
+# Install python-decouple to use config method
+# Do not forget to create .env file in the root directory of the project
+# and add the following information
+# EMAIL_HOST_USER = 'emailaddress@gmail.com'
+# EMAIL_HOST_PASSWORD = 'app password created in google account'
+
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
